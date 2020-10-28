f063e844b4478390f9158cd1c9edc8433e78f353
Chun-Fung Wong <chunefung@gmail.com>
2020-10-29 00:49:23 +0800

    test

---
+++ test/job_test.py
@@ -0,0 +1,54 @@
+import unittest
+
+from slurmpter import SlurmJob
+
+expected_TestSlurmJob_test_build_output = """#!/bin/bash
+#SBATCH --job-name=test_slurmjob_build
+#SBATCH --output=slurm/output/test_slurmjob_build.output
+#SBATCH --error=slurm/error/test_slurmjob_build.error
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=16G
+#SBATCH --sbatch_option_0=sbatch_xoption_0
+#SBATCH --sbatch_option_1=sbatch_xoption_1
+
+extra_line_0
+extra_line_1
+
+module load module_0
+module load module_1
+
+srun --srun_option_0=srun_xoption_0 --srun_option_1=srun_xoption_1 executable --arg arg_0 &
+srun --srun_option_0=srun_xoption_0 --srun_option_1=srun_xoption_1 executable --arg arg_1 &
+wait
+"""
+
+
+class TestSlurmJob(unittest.TestCase):
+
+    def test_build(self):
+        error = "slurm/error"
+        output = "slurm/output"
+        submit = "slurm/submit"
+
+        job = SlurmJob(name="test_slurmjob_build",
+                       executable="executable",
+                       submit=submit,
+                       output=output,
+                       error=error,
+                       nodes=1,
+                       ntasks_per_node=1,
+                       cpus_per_task=4,
+                       mem_per_node="16G",
+                       extra_sbatch_options=["sbatch_option_0=sbatch_xoption_0",
+                                             "sbatch_option_1=sbatch_xoption_1"],
+                       extra_srun_options=["srun_option_0=srun_xoption_0",
+                                           "srun_option_1=srun_xoption_1"],
+                       extra_lines=["extra_line_0", "extra_line_1"],
+                       modules=["module_0", "module_1"],
+                       arguments=["--arg arg_0", "--arg arg_1"])
+        job.build(fancyname=False)
+        with open("slurm/submit/test_slurmjob_build.submit", "r") as f:
+            data = f.read()
+        self.assertEqual(data, expected_TestSlurmJob_test_build_output)
